60e56d065673f57b1cd8619a6baea91f7b4604ca
Nurnehir <120215112+Nurnehir@users.noreply.github.com>
2026-01-18 22:11:39 +0300

    Update train_texture_cnn.m

--- matlab/train_texture_cnn.m
+++ matlab/train_texture_cnn.m
@@ -1,7 +1,21 @@
-dataPath = fullfile(pwd, 'dataset');
+% train_texture_cnn.m
+% Texture classification with MobileNetV2 (Transfer Learning)
+% Dataset: dataset/{wood,metal,fabric,plastic}
 
-imds = imageDatastore(dataPath, 'IncludeSubfolders', true, 'LabelSource', 'foldernames');
+clear; clc;
+
+dataPath = fullfile(pwd, 'dataset');
+imds = imageDatastore(dataPath, ...
+    'IncludeSubfolders', true, ...
+    'LabelSource', 'foldernames');
+
+if isempty(imds.Files)
+    error("Dataset not found. Expected images under: %s", dataPath);
+end
+
 numClasses = numel(categories(imds.Labels));
+disp("Classes:"); disp(categories(imds.Labels));
+disp("Counts:");  disp(countEachLabel(imds));
 
 [imdsTrain, imdsVal] = splitEachLabel(imds, 0.8, 'randomized');
 
@@ -15,13 +29,18 @@ augmenter = imageDataAugmenter( ...
     'RandXReflection', true, ...
     'RandScale', [0.9 1.1]);
 
-augTrain = augmentedImageDatastore(inputSize(1:2), imdsTrain, 'DataAugmentation', augmenter);
+augTrain = augmentedImageDatastore(inputSize(1:2), imdsTrain, ...
+    'DataAugmentation', augmenter);
+
 augVal = augmentedImageDatastore(inputSize(1:2), imdsVal);
 
+% --- Transfer learning: replace last layers safely by class type ---
 lgraph = layerGraph(net);
-fcName = find_layer_name(lgraph, 'nnet.cnn.layer.FullyConnectedLayer');
-softmaxName = find_layer_name(lgraph, 'nnet.cnn.layer.SoftmaxLayer');
-className = find_layer_name(lgraph, 'nnet.cnn.layer.ClassificationLayer');
+
+fcName       = find_layer_name_any(lgraph, ["nnet.cnn.layer.FullyConnectedLayer"]);
+softmaxName  = find_layer_name_any(lgraph, ["nnet.cnn.layer.SoftmaxLayer"]);
+classLayName = find_layer_name_any(lgraph, ["nnet.cnn.layer.ClassificationLayer","nnet.cnn.layer.ClassificationOutputLayer"]);
+
 
 newFc = fullyConnectedLayer(numClasses, 'Name', 'new_fc', ...
     'WeightLearnRateFactor', 10, 'BiasLearnRateFactor', 10);
@@ -30,8 +49,9 @@ newClass = classificationLayer('Name', 'new_classoutput');
 
 lgraph = replaceLayer(lgraph, fcName, newFc);
 lgraph = replaceLayer(lgraph, softmaxName, newSoftmax);
-lgraph = replaceLayer(lgraph, className, newClass);
+lgraph = replaceLayer(lgraph, classLayName, newClass);
 
+% --- Training options ---
 options = trainingOptions('adam', ...
     'InitialLearnRate', 1e-4, ...
     'MaxEpochs', 6, ...
@@ -42,46 +62,56 @@ options = trainingOptions('adam', ...
     'Verbose', false, ...
     'Plots', 'training-progress');
 
+% --- Train ---
 trainedNet = trainNetwork(augTrain, lgraph, options);
 
+% --- Save results ---
 if ~exist('results', 'dir')
     mkdir('results');
 end
 
 save(fullfile('results', 'trainedNet.mat'), 'trainedNet');
 
+% --- Validate ---
 YPred = classify(trainedNet, augVal);
 YTrue = imdsVal.Labels;
 acc = mean(YPred == YTrue);
 disp(['Val Accuracy: ', num2str(acc)]);
 
-figure;
+% Confusion matrix
+fig1 = figure('Visible','off');
 confusionchart(YTrue, YPred);
-exportgraphics(gcf, fullfile('results', 'confusion_matrix.png'));
-close(gcf);
+exportgraphics(fig1, fullfile('results', 'confusion_matrix.png'));
+close(fig1);
 
+% Sample predictions figure
 numSamples = 12;
 valCount = numel(imdsVal.Files);
 sampleIdx = randperm(valCount, min(numSamples, valCount));
-figure;
+
+fig2 = figure('Visible','off');
 for i = 1:numel(sampleIdx)
     idx = sampleIdx(i);
     img = readimage(imdsVal, idx);
-    img = imresize(img, inputSize(1:2));
-    pred = classify(trainedNet, img);
-    subplot(3, 4, i);
-    imshow(img);
-    title(sprintf('T:%s P:%s', string(imdsVal.Labels(idx)), string(pred)));
-end
-exportgraphics(gcf, fullfile('results', 'sample_predictions.png'));
-close(gcf);
+    imgResized = imresize(img, inputSize(1:2));
+    pred = classify(trainedNet, imgResized);
 
+    subplot(3, 4, i);
+    imshow(imgResized);
+    title(sprintf('T:%s  P:%s', string(imdsVal.Labels(idx)), string(pred)));
+end
+exportgraphics(fig2, fullfile('results', 'sample_predictions.png'));
+close(fig2);
+
+disp("Done. Files saved to results/: trainedNet.mat, confusion_matrix.png, sample_predictions.png");
+
+% ---------- helper ----------
 function name = find_layer_name(lgraph, className)
 layers = lgraph.Layers;
-match = arrayfun(@(l) isa(l, className), layers);
-idx = find(match, 1, 'last');
+layerClasses = arrayfun(@(l) string(class(l)), layers);
+idx = find(layerClasses == string(className), 1, 'last');
 if isempty(idx)
-    error('Layer not found: %s', className);
+    error("Layer not found: %s", className);
 end
 name = layers(idx).Name;
 end
